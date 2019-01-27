4b6b4a28cd787c15919625f8022f4055677dc217
real-quantdog <ericbell271@gmail.com>
2019-01-27 15:36:44 -0500

    Commented to the Main Script

--- StockDataLoad/Main.py
+++ StockDataLoad/Main.py
@@ -5,27 +5,35 @@ Created on Fri Jan 25 20:22:28 2019
 @author: Eric Bell
 """
 
+##Add your connection to connect the DB.
+from db_loadfiles.connect_DB import BlueDream
+
 
 #PullData is where we are getting the data; If It has insert in the name, it a SQL Transaction
 from PullData.GetDataHistoric import HistoricalPricingInsert
 from PullData.GetDataToday import TodayPricingInsert
-from PullData.GetRobinhoodPrices import GetCurrentPrices
 
-
+###This universe can be modified. I also have functions that can be added to add more stocks. 
 from UniverseData.universe import Faber_Ticker, SPY_Only_Universe
 
-from db_loadfiles.connect_DB import BlueDream
+
+#The Analytics folder is where all of the trend calculations are. 
 from Analytics.trendy import  supres, gentrends, segtrends, minitrends, iterlines
 from Analytics.LocalMaxMin import get_max_min, find_patterns,plot_minmax_patterns
-#Build DataBase
+
+
+
 
 #Universe = 
-Universe = Faber_Ticker
+Universe = Faber_Ticker #Universe can be modified in the UniverseData.universe file. 
 
 
-HistoricalPricingInsert(Universe , '1h')
-TodayPricingInsert(Universe)
-GetCurrentPrices(Universe )
+#Build DataBase
+HistoricalPricingInsert(Universe , '1h') #This can be 1D to get daily date. 
+#You do not have to run this everyday. This builds the historical data base for a year. This can be configured in 
+#PullData.HistoricDataPullDates with the GetYearWindow function.
+TodayPricingInsert(Universe) #This will insert the data in for the day.
+
 
 
 from DBQuery.BlueDream import GetPrice
@@ -45,12 +53,12 @@ for stock in Universe :
     df.set_index(['ticker', 'DateTime' ])
     df.drop('Date', axis=1, inplace=True)
     df.head()
-    max_min =get_max_min(df, smoothing, window)
-    df.reset_index()['close'].plot()
-    plt.scatter(max_min.index, max_min.values, color = 'orange', alpha = 0.50)
+    max_min =get_max_min(df, smoothing, window)#this function calculates the min and max of the stocks. 
+    df.reset_index()['close'].plot()##Run these two functions together
+    plt.scatter(max_min.index, max_min.values, color = 'orange', alpha = 0.50)##to complete the plot
 #    df.ema =df.close.ewm(com=0.5).mean()
 #    df.head()
-    ema=df.close.ewm(span=20,adjust=False).mean()
-    patterns  =find_patterns(max_min)
+    ema=df.close.ewm(span=20,adjust=False).mean() ###Calculate the ema
+    patterns  =find_patterns(max_min) ###Find stock patterns. 
     plot_minmax_patterns(df, max_min, patterns, stock, window, ema)
 
--- StockDataLoad/db_loadfiles/connect_DB.py
+++ StockDataLoad/db_loadfiles/connect_DB.py
@@ -8,5 +8,5 @@ Created on Sat Sep 29 22:26:37 2018
 
 from psycopg2 import connect
     
-BlueDream = connect(dbname="BlueDreamDB", user="",  password = '')
+BlueDream = connect(dbname="BlueDreamDB", user="postgres",  password = 'ew1234')
 BlueDream.autocommit = True
